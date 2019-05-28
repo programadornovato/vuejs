dee5790b493f78998b14c14cdabcc6c9792fc2de
Programador Novato <info@programadornovato.com>
2019-05-28 17:22:30 -0500

    14.-Vue js 2 tutorial español 👀 [Actions,mapActions,mapState,]👀👀

---
+++ datos.js
@@ -0,0 +1,5 @@
+[
+    {"nombre":"Java","url":"https://www.java.com/es/","id":1},
+    {"nombre":"PHP","url":"https://php.net/","id":2},
+    {"nombre":"C++","url":"https://www.bloodshed.net","id":3}
+]
